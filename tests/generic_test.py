af278739b0a443b1f1354c090e23c25ddb89fdcb
anstepp <stepp.aaron@gmail.com>
2026-06-10 09:37:45 -0500

    updates to pyproject link tests; see how they run

--- tests/generic_test.py
+++ tests/generic_test.py
@@ -1,4 +1,16 @@
 import pytest 
 
+from src.main import arg_list
+
 def test_launch():
 	assert(True)
+
+def test_addition():
+	sum = 2+2
+	assert 2+2 == 4
+
+def test_argument_list():
+	test_list = [1,2]
+	return_list = arg_list(test_list)
+	assert return_list[0] == test_list[0]
+	assert return_list[1] == test_list[1]
